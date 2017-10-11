6a47ee11b8756ab56617e6e3595c9c0afa3c3d1b
Matthias Riedl <matthias.riedl@frequentis.com>
2017-10-11 15:51:11 +0200

    Fixed issue where JSON property was not formatted properly

--- aggregated-geocoder-model/src/main/java/com/github/ianmcderp/api/model/Geolocation.java
+++ aggregated-geocoder-model/src/main/java/com/github/ianmcderp/api/model/Geolocation.java
@@ -1,6 +1,9 @@
 package com.github.ianmcderp.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Geolocation {
+    @JsonProperty("formatted_address")
     private final String formattedAddress;
 
     public Geolocation(final String formattedAddress) {
--- aggregated-geocoder-service/src/main/java/com/github/ianmcderp/Application.java
+++ aggregated-geocoder-service/src/main/java/com/github/ianmcderp/Application.java
@@ -1,4 +1,4 @@
-package com.github.ianmcderp.service;
+package com.github.ianmcderp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
